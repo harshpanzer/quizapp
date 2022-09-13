5647e8efa8ae632f4795c9cf57607dd10cec7c95
harshpanzer <harshpathakagra@gmail.com>
2022-09-14 00:08:35 +0530

    initial commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "C is made by?",
+            "Which one is not a programming language?",
+            "Currency of India?",
+            "Capital of UP?"
+    };
+
+    public static String choices[][] = {
+            {"Dennis Ritchie","Hoffman","Richard ","Walter White"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Dollars","Rupee","Denar","Euro"},
+            {"Lucknow","Agra","Mathura","Banaras"}
+    };
+
+    public static String correctAnswers[] = {
+            "Dennis Ritchie",
+            "Notepad",
+            "Rupee",
+            "Lucknow"
+    };
+
+}
